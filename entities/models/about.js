4362474c22af48384feaa783691cd4924e1d225f
jun <toinin@naver.com>
2021-04-07 14:35:17 +0900

    [ADD] Model - About

---
+++ entities/models/about.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "about",
+    {
+      username: {
+        type: DataTypes.STRING(20),
+        unique: true,
+        primaryKey: true,
+      },
+      phone_number: {
+        type: DataTypes.STRING(100),
+      },
+      email: {
+        type: DataTypes.STRING(10),
+      },
+      git_url: {
+        type: DataTypes.STRING(20),
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
--- entities/models/index.js
+++ entities/models/index.js
@@ -13,5 +13,8 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 //orm sequelize model:generate --name User --attributes name:string,email:string,password:string
 db.User = require("./user")(sequelize, Sequelize);
+db.About = require("./about")(sequelize, Sequelize);
 
+db.User.hasOne(db.About, { foreignKey: "email", targetKey: "email" });
+db.About.belongsTo(db.User, { foreignKey: "email" });
 module.exports = db;

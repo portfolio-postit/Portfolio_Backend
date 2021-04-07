8de1e84c838f82d4b7d41792b113bd91bbf65b6e
jun <toinin@naver.com>
2021-04-07 14:19:06 +0900

    [Add] new routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express")();
 const user = require("./users");
-
+const about = require("./about");
 router.use("/user", user);
-
+// router.use("/skill", skill);
+// router.use("project", project);
+router.use("/about", about);
 module.exports = router;

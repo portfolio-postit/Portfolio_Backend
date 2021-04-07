1a686bf03b824c34642b07caa5d86542a8e33434
jun <toinin@naver.com>
2021-04-07 18:50:43 +0900

    [ADD] controller - skill base

---
+++ src/controller/skill/controller.js
@@ -0,0 +1,9 @@
+const { User } = require("../../entities/models");
+const query = require("./query");
+const AWS = require("aws-sdk");
+
+const write = async (req, res) => {};
+
+module.exports = {
+  write,
+};

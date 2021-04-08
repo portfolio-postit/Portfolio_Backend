4930b6d8a1b67e092359bf45412de468b84a2217
jun <toinin@naver.com>
2021-04-08 23:36:16 +0900

    [ADD] About - delete

--- src/controller/about/controller.js
+++ src/controller/about/controller.js
@@ -1,4 +1,5 @@
 const { About } = require("../../entities/models");
+const about = require("../../entities/models/about");
 const query = require("./query");
 const createAbout = async (req, res, next) => {
   try {
@@ -16,7 +17,7 @@ const createAbout = async (req, res, next) => {
     res.status(200).end();
   } catch (e) {
     console.log(e);
-    res.status(409).end();
+    res.status(400).end();
   }
 };
 
@@ -26,11 +27,24 @@ const readAbout = async (req, res) => {
     res.status(200).json(about);
   } catch (e) {
     console.log(e);
-    res.status(409).end();
+    res.status(400).end();
   }
 };
 
+const deleteAbout = async (req, res) => {
+  try {
+    const user = await query.findOneByEmail(req.decoded.email);
+    if (!user) res.status(400).end();
+    about.destroy({
+      where: { username: user.name },
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).end();
+  }
+};
 module.exports = {
   createAbout,
   readAbout,
+  deleteAbout,
 };

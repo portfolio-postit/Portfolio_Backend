cbedc525caca4dd6924bbfd97d8000b3e9824b84
jun <toinin@naver.com>
2021-04-08 10:10:51 +0900

    [ADD] skill - getlist

--- src/controller/skill/controller.js
+++ src/controller/skill/controller.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const { Skill } = require("../../entities/models");
 const query = require("./query");
 
@@ -18,6 +19,28 @@ const write = async (req, res) => {
   }
 };
 
+const showSkill = async (req, res) => {
+  try {
+    const skill = await query.findByEmail(req.query.email);
+    if (!skill) res.status(400);
+    const response = _.map(skill, (e) => {
+      e.url = process.env.S3URL + e.file_name;
+      return _.pick(e, [
+        "id",
+        "url",
+        "origin_name",
+        "skill_name",
+        "skill_score",
+        "email",
+      ]);
+    });
+    res.status(200).send({ response });
+  } catch (e) {
+    console.log(e);
+    res.status(400).end();
+  }
+};
 module.exports = {
   write,
+  showSkill,
 };

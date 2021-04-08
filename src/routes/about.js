7fbcd1edaab9b55a10532a9744aba0cb67f8d44d
jun <toinin@naver.com>
2021-04-09 08:45:27 +0900

    [FIX] routes - upload image

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,7 +1,15 @@
 const router = require("express")();
 const controller = require("../controller/about/controller");
 const auth = require("../middleware/auth");
-router.post("/", auth.authMiddleware, controller.createAbout);
+const type = require("../middleware/type");
+var multer = require("multer");
+var upload = multer();
+router.post(
+  "/",
+  auth.authMiddleware,
+  upload.single("image"),
+  controller.createAbout
+);
 router.get("/:email", controller.readAbout);
 router.delete("/", auth.authMiddleware, controller.deleteAbout);
 module.exports = router;

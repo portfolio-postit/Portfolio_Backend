4524199adaaf20ab52a1f40f881a6ae7221875be
jun <toinin@naver.com>
2021-04-08 21:27:21 +0900

    [ADD] routes

--- src/routes/project.js
+++
@@ -1,7 +0,0 @@
-const router = require("express")();
-const controller = require("../controller/skill/controller");
-const auth = require("../middleware/auth");
-router.post("/", auth.authMiddleware);
-router.get("/", controller.showSkill);
-router.delete("/", auth.authMiddleware);
-module.exports = router;
--- src/routes/skill.js
+++ src/routes/skill.js
@@ -1,15 +1,20 @@
 const router = require("express")();
+var multer = require("multer");
+var upload = multer();
 const controller = require("../controller/skill/controller");
 const auth = require("../middleware/auth");
-const multer = require("../middleware/multer");
+const type = require("../middleware/type");
+// const s3 = require("../controller/s3/controller");
+// const multer = require("../middleware/multer");
 const { route } = require("./users");
 router.post(
   "/",
   auth.authMiddleware,
-  multer.upload.single("image"),
+  upload.single("image"),
+  type.typeMiddleware,
   controller.write
 );
 router.get("/", controller.showAllSkill);
-router.get("/Type", controller.showTypeSkill);
-router.delete("/", auth.authMiddleware, multer.deleteS3);
+router.get("/type", controller.showTypeSkill);
+// router.delete("/", auth.authMiddleware, s3.deleteS3);
 module.exports = router;

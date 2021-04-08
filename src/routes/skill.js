1abd4266262a528cf886a2defa00b3c6f287198d
jun <toinin@naver.com>
2021-04-08 10:11:11 +0900

    [ADD] routes - skill

--- src/routes/skill.js
+++ src/routes/skill.js
@@ -3,12 +3,10 @@ const controller = require("../controller/skill/controller");
 const auth = require("../middleware/auth");
 const upload = require("../middleware/multer");
 router.post(
-  "/upload",
+  "/",
   auth.authMiddleware,
   upload.upload.single("image"),
   controller.write
 );
-router.get("/test", function (req) {
-  console.log(req.body.id);
-});
+router.get("/", controller.showSkill);
 module.exports = router;

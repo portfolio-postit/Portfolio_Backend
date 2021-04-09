68e0e2582d992abc180844c70bb06b49f02793fc
jun <toinin@naver.com>
2021-04-10 01:26:17 +0900

    [ADD] controller - skil - changeskill

--- src/controller/skill/controller.js
+++ src/controller/skill/controller.js
@@ -3,7 +3,7 @@ const { Skill } = require("../../entities/models");
 const repositories = require("../../entities/repositories/skill");
 const { uploadFile } = require("./service");
 
-const write = async (req, res) => {
+const write = async (req, res, next) => {
   try {
     const { skill_name, skill_score, skill_type } = req.body;
     const uuidname = await uploadFile(req.file);
@@ -73,8 +73,22 @@ const showTypeSkill = async (req, res) => {
   }
 };
 
+const changeSkill = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const skill = await repositories.findOneById(id);
+    if (!skill) res.status(400).end();
+    const { skill_name, skill_score, skill_type } = req.body;
+    Skill.update({ skill_type, skill_name, skill_score }, { where: { id } });
+    res.status(200).end();
+  } catch (e) {
+    console.log(e);
+    res.status(400).end();
+  }
+};
 module.exports = {
   write,
   showAllSkill,
   showTypeSkill,
+  changeSkill,
 };

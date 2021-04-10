51f135351810f128034ada8ed688c3f0daedb30e
jun <toinin@naver.com>
2021-04-10 21:14:04 +0900

    [ADD] service - about - uploadfile

---
+++ src/controller/about/service.js
@@ -0,0 +1,27 @@
+const s3 = require("../../config/s3");
+const uuid = require("uuid4");
+const { extname } = require("path");
+
+const uploadFile = async (file) => {
+  try {
+    const blob = file.buffer;
+    const filename = uuid();
+    const uuidname = filename + extname(file.originalname);
+    const params = {
+      Bucket: "toinin",
+      Key: uuidname,
+      Body: blob,
+    };
+
+    s3.upload(params, function (err, data) {
+      console.log(err, data);
+    });
+    return uuidname;
+  } catch (e) {
+    throw e;
+  }
+};
+
+module.exports = {
+  uploadFile,
+};

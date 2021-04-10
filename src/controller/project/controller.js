22d7accfdd0e610dbf7e7763699f13d3c08b0175
jun <toinin@naver.com>
2021-04-10 20:38:00 +0900

    [FIX] controller - project - function

--- src/controller/project/controller.js
+++ src/controller/project/controller.js
@@ -1,29 +1,16 @@
-const query = require("./query");
-const uuid = require("uuid4");
-const { extname } = require("path");
-const s3 = require("../../config/s3");
-const { Project, Project_tag } = require("../../entities/models");
+const projectRepositories = require("../../entities/repositories/project");
+const tagRepositories = require("../../entities/repositories/project_tag");
+const userRepositroes = require("../../entities/repositories/user");
+const service = require("./service");
 const _ = require("lodash");
 
 const createProject = async (req, res, next) => {
   try {
-    console.log(req.body);
     const { link, project_title, project_content, project_tag } = req.body;
-    console.log(project_tag);
-    const user = await query.findOneByEmail(req.decoded.email);
+    const user = await userRepositroes.findOneByEmail(req.decoded.email);
     if (!user) res.status(400).end();
-    const blob = req.file.buffer;
-    const filename = uuid();
-    const uuidname = filename + extname(req.file.originalname);
-    const params = {
-      Bucket: "toinin",
-      Key: uuidname,
-      Body: blob,
-    };
 
-    s3.upload(params, function (err, data) {
-      console.log(err, data);
-    });
+    const uuidname = service.uploadFile(req.file);
 
     project = await Project.create({
       email: user.email,
@@ -32,12 +19,14 @@ const createProject = async (req, res, next) => {
       project_title,
       project_content,
     });
+
     await project_tag.map((e) => {
       Project_tag.create({
         tag: e,
         projectId: project.id,
       });
     });
+
     res.status(200).end();
   } catch (e) {
     console.log(e);
@@ -47,8 +36,8 @@ const createProject = async (req, res, next) => {
 
 const addTag = async (req, res, next) => {
   try {
-    const user = await query.findOneByEmail(req.decoded.email);
-    const project = await query.findOneByProjectId(req.params.id);
+    const user = await userRepositroes.findOneByEmail(req.decoded.email);
+    const project = await projectRepositories.findOneById(req.params.id);
     if (!user) res.status(400).end();
     if (user.email != project.email) res.stats(400).end();
     const { project_tag } = req.body;
@@ -67,20 +56,21 @@ const addTag = async (req, res, next) => {
 
 const readDetailProject = async (req, res) => {
   try {
-    const project = await query.findOneByProjectId(req.params.id);
-    const project_tag = await query.findAllByTagId(project.id);
-    const data = project;
-    data.dataValues.url = process.env.S3URL + project.file_name;
-    data.dataValues.tag = project_tag;
-    res.status(200).json({ data });
+    const project = await projectRepositories.findOneById(req.params.id);
+    const project_tag = await tagRepositories.findAllByProjectId(project.id);
+    const response = project;
+    response.dataValues.url = process.env.S3URL + project.file_name;
+    response.dataValues.tag = project_tag;
+    res.status(200).json({ response });
   } catch (e) {
     console.log(e);
     res.status(400).end();
   }
 };
+
 const readAllRroject = async (req, res) => {
   try {
-    const project = await query.findAllByProjectEmail(req.params.email);
+    const project = await projectRepositories.findAllByEmail(req.params.email);
     res.status(200).json({ project });
   } catch (e) {
     console.log(e);
@@ -90,8 +80,8 @@ const readAllRroject = async (req, res) => {
 
 const deleteProject = async (req, res, next) => {
   try {
-    const user = await query.findOneByEmail(req.decoded.email);
-    const project = await query.findOneByProjectId(req.params.id);
+    const user = await userRepositroes.findOneByEmail(req.decoded.email);
+    const project = await projectRepositories.findOneById(req.params.id);
     if (user.email != project.email) res.status(400).end();
 
     await Project_tag.destroy({
@@ -116,18 +106,21 @@ const deleteProject = async (req, res, next) => {
 
 const deleteTag = async (req, res, next) => {
   try {
-    const user = await query.findOneByEmail(req.decoded.email);
-    const project_tag = await Project_tag.findOne({ id: req.params.id });
-    const project = await Project.findOne({ id: project_tag.projectId });
+    const user = await userRepositroes.findOneByEmail(req.decoded.email);
+    const project_tag = await tagRepositories.findOneById(req.params.id);
+    const project = await projectRepositories.findOneById(
+      project_tag.projectId
+    );
     if (user.email != project.email) res.status(400).end();
     await Project_tag.destroy({
-      where: { projectId: project.id },
+      where: { id: project_tag.id },
     });
   } catch (e) {
     console.log(e);
     res.status(400).end();
   }
 };
+
 module.exports = {
   createProject,
   readDetailProject,

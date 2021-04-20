9d5cd59dc7afa8afa7389328d5a09c550a20f5c9
jun <toinin@naver.com>
2021-04-20 09:43:18 +0900

    Add routes - question

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const about = require("./about");
 const skill = require("./skill");
 const project = require("./project");
 const main = require("./main");
+const question = require("./question");
 
 router.use("/main", main);
 router.use("/user", user);
 router.use("/skill", skill);
 router.use("/project", project);
 router.use("/about", about);
+router.use("/question", question);
 module.exports = router;
